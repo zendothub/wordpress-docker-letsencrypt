05db7b69a79d9ab13cdd4e2fdf70019697b400c4
abdurrakib321 <abdur@zendot.in>
2024-11-25 18:13:36 +0530

    script added for installing docker

---
+++ master.sh
@@ -0,0 +1,10 @@
+sudo yum update -y
+sudo yum install docker -y
+sudo systemctl start docker
+sudo systemctl enable docker
+sudo usermod -aG docker ec2-user
+sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+mkdir -p wp-data
+mkdir -p letsencrypt
