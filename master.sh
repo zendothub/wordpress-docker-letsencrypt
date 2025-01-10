3229be42bcfdfb1a741b5b42641e892abb81d29a
abdurrakib321 <abdur@zendot.in>
2025-01-10 13:07:45 +0530

    master script is modified for ubunto

--- master.sh
+++ master.sh
@@ -1,10 +1,11 @@
-sudo yum update -y
-sudo yum install docker -y
+sudo apt update -y
+sudo apt install docker.io -y
 sudo systemctl start docker
 sudo systemctl enable docker
-sudo usermod -aG docker ec2-user
+sudo usermod -aG docker $USER
 sudo curl -L "https://github.com/docker/compose/releases/download/v2.0.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-
+docker --version
+docker-compose --version
 mkdir -p wp-data
 mkdir -p letsencrypt
